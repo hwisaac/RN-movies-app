82d8363f88b3cb3e78b9c0f2852cd3e99c418b58
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-10-09 12:16:30 +0900

    AppLoading

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style='auto' />
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text> Demo! 👋</Text>
     </View>
   );
 }
--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 `create-react-native-app` 으로 만든 영화 검색 앱
 
+App loading part1 
+## Asset 을 Preload 하기
 
-## 에러
+> svg, 로고와 같은 이미지를 preload 해보기
 
+expo-asset 을 설치합니다. font 와 asset 을 preload 하게 해줍니다. 
